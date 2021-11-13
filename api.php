d17418169235a0c95d6df9d8eb5a4211abe0b2fa
Thqmaz <t.groenendijk.tg@gmail.com>
2021-11-13 22:32:18 +0100

    Api key gefixt op de index.php

--- api.php
+++ api.php
@@ -2,20 +2,22 @@
 include 'api/objects/Access.php';
 $access = new Access($dbConn);
 if (isset($_GET['action'])) {
-    if(isset($_GET['apikey'])) {
-        if(!$access->validateAPIkey($_GET['apikey'])) {
+    if (!isset($_POST['apikey'])) {
+        if (isset($_GET['apikey'])) {
+            if (!$access->validateAPIkey($_GET['apikey'])) {
+                http_response_code(404);
+                echo json_encode(
+                    array("message" => "Ongeldige API key opgegeven")
+                );
+                exit;
+            }
+        } else {
             http_response_code(404);
             echo json_encode(
                 array("message" => "Ongeldige API key opgegeven")
             );
             exit;
         }
-    } else {
-        http_response_code(404);
-        echo json_encode(
-            array("message" => "Ongeldige API key opgegeven")
-        );
-        exit;
     }
     switch ($_GET['action']) {
         case 'createProduct':
@@ -35,4 +37,3 @@ if (isset($_GET['action'])) {
             break;
     }
 }
-?>
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-require 'api.php';
+require_once 'api.php';
+require_once 'api/objects/Access.php';
+$access = new Access($dbConn);
+if (isset($_POST['submitApiKey'])) {
+    if (!$access->validateAPIkey($_POST['apikey'])) {
+        die("Ongeldige API key ingevoerd");
+    }
+    $apiKey = $_POST['apikey'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,9 +20,22 @@ require 'api.php';
 </head>
 
 <body>
+    <h2>API Key</h2>
+    <form action="#" method="POST">
+        Api Key<input type="text" name="apikey"><br>
+        <input type="submit" name="submitApiKey" value="Bevestig">
+    </form>
+    <?php if (isset($_POST['apikey'])) {
+        echo "<h2 style='color:lime'>API Key ingesteld</h2>";
+    }
+    ?>
+    <br>
+    <br>
+    <br>
     <h2>Create product</h2>
     <form action="api.php" method="get">
         <input type="hidden" name="action" value="createProduct">
+        <input type="hidden" name="apikey" value="<?php echo $_POST['apikey']; ?>">
         Naam<input type="text" name="name"><br>
         Beschrijving<input type="text" name="desc"><br>
         Prijs<input type="text" name="price"><br>
@@ -26,6 +47,7 @@ require 'api.php';
     <h2>Delete product</h2>
     <form action="api.php" method="get">
         <input type="hidden" name="action" value="deleteProduct">
+        <input type="hidden" name="apikey" value="<?php echo $_POST['apikey']; ?>">
         ID<input type="text" name="id"><br>
         <input type="submit" value="Bevestig">
     </form>
@@ -35,6 +57,7 @@ require 'api.php';
     <h2>Update product</h2>
     <form action="api.php" method="get">
         <input type="hidden" name="action" value="updateProduct">
+        <input type="hidden" name="apikey" value="<?php echo $_POST['apikey']; ?>">
         ID<input type="text" name="id"><br>
         Naam<input type="text" name="name"><br>
         Beschrijving<input type="text" name="desc"><br>
